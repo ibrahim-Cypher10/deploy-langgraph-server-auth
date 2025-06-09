afd2954798af5c3cb027538f3ccd49d8514b47b8
Kenneth Liao <32213208+kenneth-liao@users.noreply.github.com>
2025-06-09 15:48:23 -0700

    removed unused and added graph visualization

--- src/rocket/graph.py
+++ src/rocket/graph.py
@@ -1,17 +1,14 @@
 from langgraph.graph import StateGraph, add_messages, END
 from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import ToolNode
-from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
+from langchain_core.messages import SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
-from typing import Annotated, List, Optional, Dict, Any
+from typing import Annotated, List
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from rocket.my_mcp.config import mcp_config
-import json
-# from composio_langchain import ComposioToolSet, App, Action
 from dotenv import load_dotenv
-import os
 from rocket.prompts.prompts import rocket_system_prompt
 
 
@@ -20,19 +17,13 @@ load_dotenv()
 
 class AgentState(BaseModel):
     messages: Annotated[List, add_messages] = []
-    video_search_result: List[dict] = []
 
 
 async def build_graph() -> CompiledStateGraph:
     builder = StateGraph(AgentState)
 
     client = MultiServerMCPClient(connections=mcp_config["mcpServers"])
-    tools1 = await client.get_tools()
-
-    # toolset = ComposioToolSet(api_key=os.environ.get("COMPOSIO_API_KEY"))
-    # tools2 = toolset.get_tools(apps=[App.GOOGLESHEETS])
-
-    tools = tools1 #+ tools2
+    tools = await client.get_tools()
 
     llm = ChatOpenAI(model="gpt-4.1-mini-2025-04-14", temperature=0.1).bind_tools(tools)
 
@@ -48,20 +39,25 @@ async def build_graph() -> CompiledStateGraph:
         if state.messages[-1].tool_calls:
             return "tools"
         return END
-    
-    # def parse_tool_output(state: AgentState) -> AgentState:
-    #     last_message = state.messages[-1]
-    #     if isinstance(last_message, ToolMessage):
-    #         state.video_search_result.append(json.loads(last_message.content))
-    #     return state
 
     builder.add_node("assistant", assistant)
     builder.add_node("tools", ToolNode(tools))
-    # builder.add_node("parse_tool_output", parse_tool_output)
 
     builder.set_entry_point("assistant")
-    builder.add_conditional_edges("assistant", assistant_router)
+    builder.add_conditional_edges("assistant", assistant_router, ["tools", END])
     builder.add_edge("tools", "assistant")
-    # builder.add_edge("parse_tool_output", "assistant")
 
     return builder.compile(checkpointer=MemorySaver())
+
+async def draw_graph():
+    from IPython.display import display, Image
+
+    graph = await build_graph()
+    display(Image(graph.get_graph().draw_mermaid_png()))
+    
+
+if __name__ == "__main__":
+    import asyncio
+    import nest_asyncio
+    nest_asyncio.apply()
+    asyncio.run(draw_graph())
